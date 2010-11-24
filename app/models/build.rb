5cd00bda8f98a651aa294b7ab10bf92658b69b0a
Michal Bugno <michal.bugno@gmail.com>
2010-11-24 15:47:15 +0100

    Build dir includes second

--- app/models/build.rb
+++ app/models/build.rb
@@ -7,7 +7,7 @@ class Build < ActiveRecord::Base
 
   def perform
     project_dir = project.build_dir
-    self.build_dir = File.join(project_dir, Time.now.strftime("%Y%m%d%H%M") + "_" + commit)
+    self.build_dir = File.join(project_dir, Time.now.strftime("%Y%m%d%H%M%S") + "_" + commit)
     command = "git clone #{project.vcs_source} \"#{self.build_dir}\""
     `#{command}`
     self.stdout = `cd #{self.build_dir} && rake 2>&1 | tee rake.log`
@@ -19,22 +19,6 @@ class Build < ActiveRecord::Base
     end
     self.save!
     project.truncate_builds!
-  end
-
-  def after(job)
-    Rails.logger.debug("Job after hook")
-    Rails.logger.debug(job.inspect)
-  end
-
-  def success(job)
-    Rails.logger.debug("Job success hook")
-    Rails.logger.debug(job.inspect)
-  end
-
-  def error(job, exception)
-    Rails.logger.debug("Job error hook")
-    Rails.logger.debug(job.inspect)
-    Rails.logger.debug(exception.inspect)
   end
 
   private
