d1b4a892d4b902eaf4d023b53a9598a429ebed4a
Georg Alexander Boe <georg.boe@gmail.com>
2010-12-13 18:22:05 +0100

    Fix LoadError in BigTuna initializer.

--- config/initializers/big_tuna.rb
+++ config/initializers/big_tuna.rb
@@ -35,5 +35,5 @@ module BigTuna
   end
 end
 
-Dir[File.join("extras", "big_tuna", "vcs", "*.rb")].each { |vcs| require_dependency(vcs) }
-Dir[File.join("extras", "big_tuna", "hooks", "*.rb")].each { |hook| require_dependency(hook) }
+Dir[File.join(Rails.root, "extras", "big_tuna", "vcs", "*.rb")].each { |vcs| require_dependency(vcs) }
+Dir[File.join(Rails.root, "extras", "big_tuna", "hooks", "*.rb")].each { |hook| require_dependency(hook) }
