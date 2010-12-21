6bdd8270abaed240adc3983f488a468bd308d09a
José Galisteo Ruiz <ceritium@gmail.com>
2010-12-21 19:20:24 +0100

    fixing

--- config/initializers/big_tuna.rb
+++ config/initializers/big_tuna.rb
@@ -39,6 +39,5 @@ module BigTuna
   end
 end
 
->>>>>>> f8f1c63... Ajax calls refactored.
 Dir[File.join(Rails.root, "extras", "big_tuna", "vcs", "*.rb")].each { |vcs| require_dependency(vcs) }
 Dir[File.join(Rails.root, "extras", "big_tuna", "hooks", "*.rb")].each { |hook| require_dependency(hook) }
