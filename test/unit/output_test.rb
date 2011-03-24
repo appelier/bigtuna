a3f226b0378d191cb926ec799466c9c1d791d065
Ben Foxall <benfoxall@gmail.com>
2011-03-24 23:59:18 +0000

    added test for output

---
+++ test/unit/output_test.rb
@@ -0,0 +1,40 @@
+require 'test_helper'
+
+class OutputTest < ActiveSupport::TestCase
+  
+  
+  OUT = BigTuna::Runner::Output::TYPE_STDOUT
+  ERR = BigTuna::Runner::Output::TYPE_STDERR
+  
+  
+  setup do
+    @output = BigTuna::Runner::Output.new 'test-dir', 'test-command'
+  end
+  
+  
+  test 'output is grouped' do
+    
+    @output.append_stdout 'a'
+    @output.append_stdout 'b'
+    @output.append_stderr 'c'
+    
+    result = @output.finish 0
+    
+    assert_equal result, [[OUT, 'ab'],[ERR,'c']]
+    
+  end
+  
+  
+  test 'linebreaks are seperated' do
+    
+    @output.append_stdout "a\nb"
+    @output.append_stderr 'c'
+    
+    result = @output.finish 0
+    
+    assert_equal result, [[OUT, 'a'],[OUT, 'b'],[ERR,'c']]
+    
+  end
+  
+  
+end
