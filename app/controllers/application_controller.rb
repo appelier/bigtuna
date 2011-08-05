48a09eedd7cec88ecb052f0831a35b3f14b935a4
Matt Nemenman <matt@verticalbrands.com>
2011-08-05 15:09:22 -0700

    adding basic HTTP auth support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
   self.append_view_path("lib/big_tuna/hooks")
   protect_from_forgery
+
+  before_filter :authenticate
+
+  private
+
+  def authenticate
+    if !BigTuna.auth_user.blank? and !BigTuna.auth_pasword.blank?
+      authenticate_or_request_with_http_basic do |user_name, password|
+        user_name == BitTuna.auth_user && password == BigTuna.auth_password
+      end
+    end
+  end
+
+
 end
