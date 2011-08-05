2aab553b78e8534478541c91503cb9b343c7084c
Matt Nemenman <matt@verticalbrands.com>
2011-08-05 15:58:30 -0700

    fixing bunch of typos and testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,16 @@ class ApplicationController < ActionController::Base
   private
 
   def authenticate
+<<<<<<< Updated upstream
     if !BigTuna.auth_user.blank? and !BigTuna.auth_password.blank?
+=======
+
+    #logger.info "User/password: #{BigTuna.auth_user} #{BigTuna.auth_password}"
+
+    if !BigTuna.auth_user.blank? && !BigTuna.auth_password.blank?
+>>>>>>> Stashed changes
       authenticate_or_request_with_http_basic do |user_name, password|
-        user_name == BitTuna.auth_user && password == BigTuna.auth_password
+        user_name == BigTuna.auth_user && password == BigTuna.auth_password
       end
     end
   end
