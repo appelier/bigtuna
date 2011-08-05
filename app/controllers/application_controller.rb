d8d4b8ff92a172e0755e15c0136a97d3b7d07fee
Matt Nemenman <matt@verticalbrands.com>
2011-08-05 16:02:06 -0700

    stash went bad, fixing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,14 +7,10 @@ class ApplicationController < ActionController::Base
   private
 
   def authenticate
-<<<<<<< Updated upstream
-    if !BigTuna.auth_user.blank? and !BigTuna.auth_password.blank?
-=======
 
     #logger.info "User/password: #{BigTuna.auth_user} #{BigTuna.auth_password}"
 
     if !BigTuna.auth_user.blank? && !BigTuna.auth_password.blank?
->>>>>>> Stashed changes
       authenticate_or_request_with_http_basic do |user_name, password|
         user_name == BigTuna.auth_user && password == BigTuna.auth_password
       end
