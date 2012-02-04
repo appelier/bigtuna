68181cbce0b975984309d09d8a204e39af1db2b5
Antek Piechnik <antek.piechnik@gmail.com>
2012-02-04 16:17:21 +0100

    Fixing step list tests

--- test/integration/step_list_test.rb
+++ test/integration/step_list_test.rb
@@ -26,6 +26,7 @@ class StepListTest < ActionController::IntegrationTest
       fill_in "Steps", :with => new_steps
       click_button "Update configuration"
     end
+    @step2.reload
     assert_equal new_name, @step2.name
     assert_equal new_steps, @step2.steps
   end
