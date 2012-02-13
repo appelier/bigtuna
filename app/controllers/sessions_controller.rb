1b2e295c988379f3d01bca7621cac5a751229469
Marcin Stecki <madsheeppl@gmail.com>
2012-02-13 17:37:12 +0100

    simple fixes, auth info

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,10 @@
 class SessionsController < ApplicationController
+ 
+  skip_before_filter :gapps_required
 
   def authenticate_gapps
     auth_hash = request.env['omniauth.auth']
-
-    session[:gapps_user] = auth_hash['user_info'].try(:[], 'email')
+    session[:gapps_user] = auth_hash['info'].try(:[], 'email')
 
     if session[:gapps_user]
       redirect_to '/'
