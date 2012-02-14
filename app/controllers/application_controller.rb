5b86c18a262ca885e0972a308a641c44c9faba53
Marcin Stecki <madsheeppl@gmail.com>
2012-02-14 09:51:08 +0100

    display some info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :gapps_required, :if => proc{ BigTuna.google_apps_domain }
+  helper_method :gapps?
 
   def gapps_required
-    redirect_to '/auth/gapps' unless session[:gapps_user]
+    redirect_to '/auth/gapps' unless gapps?
+  end
+
+  private
+
+  def gapps?
+    session[:gapps_user] = "marcin.stecki@netguru.pl"
+    session[:gapps_user].present?
   end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,17 @@
 class SessionsController < ApplicationController
- 
+
   skip_before_filter :gapps_required
 
   def authenticate_gapps
     auth_hash = request.env['omniauth.auth']
     session[:gapps_user] = auth_hash['info'].try(:[], 'email')
 
-    if session[:gapps_user]
+    if gapps?
       redirect_to '/'
     else
       render :text => '401 Unauthorized', :status => 401
     end
+
   end
 
 end
